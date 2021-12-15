16b6356e144642c550447103f725fb7e3c32d182
nhatnguyen9813 <nguyenkimnhat130898@gmail.com>
2021-12-16 04:39:57 +1100

    setup main pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Footer from './components/Footer'
 // Pages
 import Homepage from './pages/Homepage'
 import Portfolio from './pages/Portfolio'
+import BlogPosts from './pages/BlogPosts'
+import AboutMe from './pages/AboutMe'
 
 export default function App() {
   return (
@@ -16,6 +18,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/blogposts" element={<BlogPosts />} />
+        <Route path="/aboutme" element={<AboutMe />} />
       </Routes>
       <Footer />
     </div>
