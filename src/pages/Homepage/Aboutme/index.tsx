0d1c1cc8ec7338d6034cbd3a4296909bc8946ccf
nhatnguyen9813 <nguyenkimnhat130898@gmail.com>
2021-12-17 00:17:18 +1100

    rework aboutme section

--- src/pages/Homepage/Aboutme/index.tsx
+++ src/pages/Homepage/Aboutme/index.tsx
@@ -8,13 +8,14 @@ export default function Aboutme() {
       <div className="SectionWidth">
         <img className="Portrait" src="/images/aboutme-portrait.jpg" alt="Aboutme Portrait" />
         <div className="Text">
-          <p className="pHeading">Nhat K. "James" Nguyen <span>&#64;nhatnguyen9813</span></p>
-          <p className="pContent">
-            <p>Welcome to my website!</p>
-            <p>My name is Nhat, but you can call me James. I'm currently working in Australia as a ReactJS frontend developer.</p>
+          <p className="pHeading Unselectable">ABOUT ME</p>
+          <article className="pContent">
+            <h3>Welcome to <span>&#64;nhatnguyen9813</span>'s ePortfolio!</h3>
+            <p>While I go by <i>Nhat K. "James" Nguyen</i>, you can just casually call me Nhat or James. I'm Vietnamese 🇻🇳 and I am currently living in Australia 🇦🇺 working as a ReactJS frontend developer.</p>
+            <p>I have 2+ years of doing web (specifically frontend/UI) development and I always look forward to building sites &amp; apps that optimal yet also eye-catching.</p>
             <p><AnchorLink href="#ContactInfo">&gt;&gt; Contact information</AnchorLink></p>
             <p><Link to="/aboutme">&gt;&gt; More about me</Link></p>
-          </p>
+          </article>
         </div>
       </div>
       
