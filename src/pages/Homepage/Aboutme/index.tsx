3613da6a9d562ad9156b663594ba2dd4b699b278
nhatnguyen9813 <nguyenkimnhat130898@gmail.com>
2021-12-16 13:42:45 +1100

    add aboutme section

--- src/pages/Homepage/Aboutme/index.tsx
+++ src/pages/Homepage/Aboutme/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import './_styles.scss'
 
 export default function Aboutme() {
   return (
     <div className="Aboutme Section">
-      abt me
+      <div className="SectionWidth">
+        <img className="Portrait" src="/images/aboutme-portrait.jpg" alt="Aboutme Portrait" />
+        <div className="Text">
+          <p className="pHeading">Nhat K. "James" Nguyen <span>&#64;nhatnguyen9813</span></p>
+          <p className="pContent">
+            <p>Welcome to my website!</p>
+            <p>My name is Nhat, but you can call me James. I'm currently working in Australia as a ReactJS frontend developer.</p>
+            <p><AnchorLink href="#ContactInfo">&gt;&gt; Contact information</AnchorLink></p>
+            <p><Link to="/aboutme">&gt;&gt; More about me</Link></p>
+          </p>
+        </div>
+      </div>
+      
     </div>
   )
 }
