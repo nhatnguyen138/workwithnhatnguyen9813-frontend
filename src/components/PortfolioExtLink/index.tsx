3c98a0f84169fd7ef8d5f03445477ed9f92eb469
nhatnguyen9813 <nguyenkimnhat130898@gmail.com>
2021-12-18 00:07:08 +1100

    add portfolio exernallink

---
+++ src/components/PortfolioExtLink/index.tsx
@@ -0,0 +1,23 @@
+import './_styles.scss'
+
+interface PortfolioExtLinkProps {
+  icon: string,
+  title: string,
+  description: string,
+  url: string
+}
+
+export default function PortfolioExtLink(props:PortfolioExtLinkProps) {
+  const { icon, title, description, url } = props
+  return (
+    <div className="PortfolioExtLink">
+      <img className="Icon" src={`/images/icon-${icon}.svg`} alt={icon} />
+      <div className="Text">
+        <div className="Title">
+          <a href={url} target="_blank" rel="noopener noreferrer">{title}</a>
+        </div>
+        <div className="Description">{description}</div>
+      </div>
+    </div>
+  )
+}
--- src/pages/Portfolio/ExternalLinks/index.tsx
+++ src/pages/Portfolio/ExternalLinks/index.tsx
@@ -1,9 +1,30 @@
+import PortfolioExtLink from '../../../components/PortfolioExtLink'
 import './_styles.scss'
 
 export default function ExternalLinks() {
   return (
     <div className="Section ExternalLinks">
-      ExternalLinks here...
+      <div className="Unselectable Heading">External Links</div>
+      <div className="List">
+        <PortfolioExtLink
+          icon="code"
+          title="Project: Human Detection App"
+          description="My own take on building a human detection app using Tensorflow.JS."
+          url="https://github.com/nhatnguyen9813/human-detection-app"
+        />
+        <PortfolioExtLink
+          icon="code"
+          title="GitHub - Crypto Example"
+          description="A JavaScript example of some basic crypto concepts."
+          url="https://github.com/nhatnguyen9813/crypto-example"
+        />
+        <PortfolioExtLink
+          icon="window"
+          title="Netlify App Archive: Blog of Nhat version 2.0"
+          description="Archive of my old website 'Blog of Nhat'."
+          url="https://blogofnhat-version2.netlify.app/"
+        />
+      </div>
     </div>
   )
 }
