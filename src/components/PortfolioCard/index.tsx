f2a58ca2f15b3ea7b469e2a1643eab383bf388fb
nhatnguyen9813 <nguyenkimnhat130898@gmail.com>
2021-12-17 16:08:18 +1100

    support screens from 350px+

--- src/components/PortfolioCard/index.tsx
+++ src/components/PortfolioCard/index.tsx
@@ -12,7 +12,7 @@ export default function PortfolioCard(props:PortfolioCardProps) {
     <div className="PortfolioCard">
       <div className="Unselectable LargeText">{action}</div>
       <div className="Text">{text}</div>
-      <div className="Technologies">
+      <div className="Unselectable Technologies">
         <span>Keys:</span>
         {technologies && technologies.map((tech, i) => {
           return ( <img key={i} className="Technology" src={`/logos/${tech}.png`} alt={`icon-${tech}`} /> )
